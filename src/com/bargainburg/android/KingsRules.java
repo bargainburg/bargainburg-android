bcb8eef5b24bdd46f3f32c5d6422bd5d69c20f05
hmafzal91 <hasan91@vt.edu>
2013-10-03 11:50:22 -0400

    Committing initial empty app
    This has the basic navigation structure of the app

---
+++ src/com/bargainburg/android/KingsRules.java
@@ -0,0 +1,28 @@
+package com.bargainburg.android;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.github.rtyley.android.sherlock.roboguice.fragment.RoboSherlockFragment;
+
+public class KingsRules extends RoboSherlockFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.simple_list_fragment, container, false);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
