beee48429292bc2f5dd76bfb12a67789be97b40a
Chris Thoma <chris.thoma@willowtreeapps.com>
2013-10-07 14:58:13 -0400

    finished skeletoning coupons api object.

--- src/com/bargainburg/android/API/Coupon.java
+++ src/com/bargainburg/android/API/Coupon.java
@@ -1,5 +1,7 @@
 package com.bargainburg.android.API;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created with IntelliJ IDEA.
  * User: christhoma
@@ -8,5 +10,28 @@ package com.bargainburg.android.API;
  * To change this template use File | Settings | File Templates.
  */
 public class Coupon {
-
+    int id;
+    @SerializedName("begin_date")
+    String beginDate;
+    @SerializedName("end_date")
+    String endDate;
+    String description;
+    boolean hidden;
+    String name;
+    @SerializedName("created_at")
+    String createdDate;
+    @SerializedName("updated_at")
+    String updatedDate;
+    @SerializedName("category_id")
+    int categoryId;
+    @SerializedName("merchant_id")
+    int merchantId;
+    @SerializedName("image_file_name")
+    String image;
+    @SerializedName("image_content_type")
+    String imageType;
+    @SerializedName("image_file_size")
+    String imageFileSize;
+    @SerializedName("image_updated_at")
+    String imageUpdatedAt;
 }
